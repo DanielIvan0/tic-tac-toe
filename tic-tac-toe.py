371be136ae790b9fc5a1be64637cc44ec10b0a1c
DanielIvan0 <danielivansaladotorres@gmail.com>
2020-05-05 13:48:34 -0500

    changePlayer function done

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,6 +3,7 @@ import random as rand
 
 # Variables globales
 players = ['', '']
+symbols = ['X', 'O']
 currentPlayer = 0
 board = [
     ['']*3,
@@ -32,7 +33,22 @@ def getPlayers():
 
 # Elegir el primer jugador
 def chooseFirstPlayer():
+    global currentPlayer
     currentPlayer = rand.randint(0, 1)
+    print(f'El primer jugador es {players[currentPlayer]}')
+
+# Iniciar el juego
+def start():
+    while True:
+        showBoard()
+        insertPlay()
+        if youWon():
+            print('Tú ganaste')
+            break
+        if fullBoard():
+            print('Tablero está lleno')
+            break
+        changePlayer()
 
 # Mostrar el tablero
 def showBoard():
@@ -41,15 +57,41 @@ def showBoard():
     print('{:^3}|{:^3}|{:^3}'.format(board[1][0], board[1][1], board[1][2]))
     print('-----------')
     print('{:^3}|{:^3}|{:^3}'.format(board[2][0], board[2][1], board[2][2]))
+
 # Manejador de turnos
+def changePlayer():
+    global currentPlayer
+    if currentPlayer == 0:
+        currentPlayer = 1
+    else:
+        currentPlayer = 0
 
 # Ingresar jugada
+def insertPlay():
+    play = input(f'{players[currentPlayer]}, inserte su jugada: ')
+    if len(play) == 2:
+        if play[0] in ('1', '2', '3') and play[1] in ('1', '2', '3'):
+            pass
+        else:
+            pass
+    else:
+        pass
 
 # Evaluar tablero
+def youWon():
+    # Retorna True si el jugador actual gana
+    # Retorna False en caso contrario
+    pass
+
+def fullBoard():
+    # Retorna True si el tablero está vacío
+    # Retorna False en caso contrario
+    pass
 
 # main function
 def main():
-    #getPlayers()
-    showBoard()
+    getPlayers()
+    chooseFirstPlayer()
+    start()
     
 main()
